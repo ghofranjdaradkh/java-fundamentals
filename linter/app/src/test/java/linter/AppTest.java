159df0217e3bd86c6ec21d44161b4c26c9bcf55c
ghofranjdaradkh <ghofran_daradkh@hotmail.com>
2023-08-19 22:27:26 +0300

    tests

--- linter/app/src/test/java/linter/AppTest.java
+++ linter/app/src/test/java/linter/AppTest.java
@@ -1,3 +1,56 @@
+package linter;
+
+import static linter.App.JavaScriptLinter;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import linter.App;
+
+import java.util.ArrayList;
+public class AppTest {
+
+    @Test
+    void JavaScriptLinterTestOneError() throws IOException {
+        Path filePath = Path.of("C:/Users/CW/java-fundamentals/linter/app/src/test/resources/oneError.js");
+        List<String> errors = JavaScriptLinter(filePath);
+        assertEquals(1, errors.size());
+
+    }
+
+    @Test
+    void JavaScriptLinterTestNoError() throws IOException {
+
+        Path filePath = Path.of("C:/Users/CW/java-fundamentals/linter/app/src/test/resources/noErrors.js");
+        List errors = JavaScriptLinter(filePath);
+        assertEquals(0, errors.size());
+    }
+    @Test
+    public void testFewErrors() throws IOException {
+        Path filePath = Path.of("C:/Users/CW/java-fundamentals/linter/app/src/test/resources/fewErrors.js");
+        List errors = JavaScriptLinter(filePath);
+        assertEquals(5, errors.size());
+
+    }
+
+    @Test
+    public void testManyErrors() throws IOException {
+        Path filePath = Path.of("C:/Users/CW/java-fundamentals/linter/app/src/test/resources/manyErrors.js");
+        List errors = JavaScriptLinter(filePath);
+        assertEquals(4, errors.size());
+    }
+
+    @Test
+    public void testEmptyFile() throws IOException {
+        Path filePath = Path.of("C:/Users/CW/java-fundamentals/linter/app/src/test/resources/emptyFile.js");
+        List<String> errors = JavaScriptLinter(filePath);
+        assertTrue(errors.isEmpty());}
+
+
+}
 
 
 
